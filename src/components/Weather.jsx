83907883a6c9277b8fcb35dd66480eb29f81e2c4
Javier <J_sedanomar@uncg.edu>
2025-05-25 14:38:43 -0400

    Hide API Key

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect , useState} from 'react'
 import DayCard from './dayCard'
+const API_KEY = import.meta.env.VITE_WEATHER_KEY;
 
 function Forecast({city}) {
     const [forecast, setForecast] = useState([]);
@@ -7,7 +8,7 @@ function Forecast({city}) {
     useEffect (() => {
         const fetchWeather = async () => {
             try {
-                const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}unitGroup=us&key=H9JYK849JBHTH5HNDD9PQCHE9&contentType=json`);
+                const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}unitGroup=us&key=${API_KEY}&contentType=json`);
                 const data = await response.json();
                 setForecast(data.days);
             } catch (error) {
